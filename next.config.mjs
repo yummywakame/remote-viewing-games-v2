4439152de5b7c15bee9f50abb8cf31bce370245b
Olivia Meiring <5927823+yummywakame@users.noreply.github.com>
2024-11-16 12:46:59 -0700

    fixed game not starting error and removed typescript

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-    // Preserve any existing configuration options you might have
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  experimental: {
+    typedRoutes: false
   }
-  
-  export default nextConfig
+}
+
+export default nextConfig
